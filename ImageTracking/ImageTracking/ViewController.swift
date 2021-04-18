83e243ec8c2434835927078294708696b0493024
Xavier Aguas <aguas.xavier2013a@gmail.com>
2021-04-18 17:19:32 -0500

    Add new project

--- ImageTracking/ImageTracking/ViewController.swift
+++ ImageTracking/ImageTracking/ViewController.swift
@@ -12,6 +12,8 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    private var planeNode: SCNNode?
+    private var imageNode: SCNNode?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,10 +25,10 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        //let scene = SCNScene(named: "art.scnassets/ship.scn")!
         
         // Set the scene to the view
-        sceneView.scene = scene
+        //sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -34,7 +36,16 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        
+        // Load reference images from "AR Resources folder"
+        
+        guard let referenceImages = ARReferenceImage.referenceImages(inGroupNamed: "Photos", bundle: Bundle.main) else {print("No images available")
+            return
+        }
 
+        
+        configuration.detectionImages = referenceImages
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -48,14 +59,47 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     // MARK: - ARSCNViewDelegate
     
-/*
+
     // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let imageAnchor = anchor as? ARImageAnchor else {
+            return
+        }
+    // 1. Load plane's scene
+        let planeScene = SCNScene(named: "art.scnassets/plane.scn")!
+        let planeNode = planeScene.rootNode.childNode(withName: "plane", recursively: true)!
+                
+        
+    // 2. Calculate size based on planeNode's bounding box.
+        let (min, max) = planeNode.boundingBox
+        let size = SCNVector3Make(max.x - min.x, max.y - min.y, max.z - min.z)
+        
+    // 3. Calculate the ratio of difference between real image and object size.
+       // Ignore Y axis because it will be pointed out of the image.
+       let widthRatio = Float(imageAnchor.referenceImage.physicalSize.width)/size.x
+       let heightRatio = Float(imageAnchor.referenceImage.physicalSize.height)/size.z
+       // Pick smallest value to be sure that object fits into the image.
+       let finalRatio = [widthRatio, heightRatio].min()!
+
+    // 4. Set transform from imageAnchor data.
+        planeNode.transform = SCNMatrix4(imageAnchor.transform)
+    
+    // 5. Animate appearance by scaling model from 0 to previously calculated value.
+    let appearanceAction = SCNAction.scale(to: CGFloat(finalRatio), duration: 0.4)
+    appearanceAction.timingMode = .easeOut
+    // Set initial scale to 0.
+    planeNode.scale = SCNVector3Make(0, 0, 0)
+    // Add to root node.
+    sceneView.scene.rootNode.addChildNode(planeNode)
+    // Run the appearance animation.
+    planeNode.runAction(appearanceAction)
+    
+    self.planeNode = planeNode
+    self.imageNode = node
+        
+        
+        
     }
-*/
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
--- ImageTracking/README.md
+++ ImageTracking/README.md
@@ -1,10 +1,17 @@
 # Seguimiento de una imagen con ARKIT
 
-### Este tutorial tiene como objetivo colocar un objeto 3D en una imagen que ha sido detectada. Se utilizará a animar cambios de rumbo y posición basandose en la transformación de la imagen.
+### Este tutorial tiene como objetivo colocar un objeto 3D en una imagen que ha sido detectada. Se utilizará para animar cambios de rumbo y posición basandose en la transformación de la imagen.
 
 ### La api de *reconocimiento de imagen* provee una herramienta para detectar imagenes en el mundo real como una ancla para un objeto 3D o como un trigger para realizar alguna acción.
 
 * Es importante mencionar que los objetos 3D deben estar correctamente alineados con la imagen y el objeto del mundo real para que las personas vean una realidad aumentada más natural. 
 
-* Una de las técnicas más utilizadas es colocar contenido virtual sobre la imágenes estáticas. La ventaja de las imágenes estáticas son comunmente analizadas porque se puede prepara el entorno (luz constante) y sobre todo porque no se mueven.
+* Una de las técnicas más utilizadas es colocar contenido virtual sobre la imágenes estáticas. La ventaja principal que poseen las imágenes estáticas es preparar el entorno (luz constante) para una mejor deteción y sobre todo porque no se mueven.
 
+#### Pero en la vida cotidiana la mayor parte de objetos que se mueven son muy atractivos para las personas. A veces se requiere que la posición del ancla sea dinámica en el mundo real para que el objeto 3D realice un seguimiento y sea más entretenido.
+
+
+
+
+
+1. Después de encontrar alguna image, agregará o actualizará un **ARImageAnchor** correspondiente que representa la posición y orientación de la imagen detectada.
